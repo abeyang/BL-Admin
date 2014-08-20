89a33d5a00366e8fed23b785d94680adfc5e7508
Abe Yang <abeyang@gmail.com>
2014-08-20 11:36:31 -0700

    added wysiwyg for contest
    still need to upload image

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ Created by Abe Yang 5/14/2014
 
 'use strict';
 
-var app = angular.module('app', ['ngSanitize']);
+var app = angular.module('app', ['ngSanitize', 'textAngular']);
 
 // CONTROLLERS
 
